7bea15fbcea92e001c31c6ca3d045969d615b70c
Shashank Jasthi <shashankjasthi@Shashanks-MacBook-Pro.local>
2017-05-16 14:38:19 -0400

    PIpes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IProduct} from '../product-list/product'
 
 @Component({
   selector: 'app-product-list',
@@ -12,7 +13,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = 'Cart';
-  products: any[]=
+  products: IProduct[]=
 [
     {
         "productId": 1,
